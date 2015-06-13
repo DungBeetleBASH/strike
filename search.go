2431796740be051b9cd240205e6018532b969f53
Robert Hill <dr.pappa@gmail.com>
2015-06-13 14:45:42 +0100

    Removed unnecessary array creation

--- search.go
+++ search.go
@@ -1,16 +1,19 @@
 package strike
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func Search(params ...string) (result interface{}, err error) {
-    args := []string{"","",""}
-    copy(args, params)
-  	fArgs := make([]interface{}, len(args))
-  	for i, v := range args {
-  	    fArgs[i] = v
+	last := len(params) - 1
+	args := make([]interface{}, 3)
+  	for i, _ := range args {
+		if (i > last) {
+			args[i] = ""
+		} else {
+	  		args[i] = params[i]
+		}
   	}
-  	query := fmt.Sprintf(api[version]["Search"], fArgs...)
+  	query := fmt.Sprintf(api[version]["Search"], args...)
 	return callApi(query)
 }
