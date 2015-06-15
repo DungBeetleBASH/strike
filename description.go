0e5304ff312f574ebdff3fd6f3448a8949f3b419
Robert Hill <dr.pappa@gmail.com>
2015-06-15 23:31:40 +0100

    Adding error handling to Description API

--- description.go
+++ description.go
@@ -3,18 +3,23 @@ package strike
 import (
     "fmt"
     "encoding/base64"
+    "errors"
 )
 
 func Description(params ...interface{}) (result map[string]interface{}, err error) {
+	l := len(params)
+	if (l == 0) {
+		return make(map[string]interface{}, 0), errors.New("Expecting at least one parameter")
+	}
 	hash, ok := params[0].(string)
 	if (!ok) {
-		return result, err
+		return make(map[string]interface{}, 0), errors.New("Expecting 1st parameter to be of type string")
 	}
 	decode := false
 	if (len(params) > 1) {
 		decode, ok = params[1].(bool)
 		if (!ok) {
-			return result, err
+			return make(map[string]interface{}, 0), errors.New("Expecting 2nd optional parameter to be of type bool")
 		}
 	}
 	query := fmt.Sprintf(api[version]["Description"], hash)
