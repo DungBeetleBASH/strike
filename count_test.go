8417e052efca85f60c32c71a62b9bcd7b6f3c800
Robert Hill <dr.pappa@gmail.com>
2015-06-16 21:03:55 +0100

    Initial unit tests

---
+++ count_test.go
@@ -0,0 +1,24 @@
+package strike
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCount(t *testing.T) {
+    ts := GetTestServer(`{"statuscode":200,"message":7352023}`)
+    defer ts.Close()
+    testMap, err := Count()
+    if assert.Nil(t, err) {
+    	statusCode, ok := testMap["statuscode"]
+    	if assert.Equal(t, true, ok) {
+    		var OK_RESPONSE float64 = 200
+    		assert.Equal(t, OK_RESPONSE, statusCode)
+    	}
+    	message, ok := testMap["message"]
+    	if assert.Equal(t, true, ok) {
+    		var COUNT float64 = 7352023
+    		assert.Equal(t, COUNT, message)
+    	}
+    }
+}
---
+++ strike_test.go
@@ -0,0 +1,21 @@
+package strike
+
+import (
+  "testing"
+  "net/http"
+  "net/http/httptest"
+  "github.com/stretchr/testify/assert"
+  "fmt"
+)
+
+func GetTestServer(responseText string) (*httptest.Server) {
+  return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    fmt.Fprintln(w, responseText)
+  }))
+}
+
+func TestSetVersion(t *testing.T) {
+  assert.Equal(t, SetVersion("v2"), true)
+  assert.Equal(t, SetVersion("v3"), false)
+}
